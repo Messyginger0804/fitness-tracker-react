65eadf3d3835881797a16afe3f28231b5275866e
JC <j.c.ashley4363@gmail.com>
2023-01-12 16:25:23 -0600

    set passwrd2 8&checked off readme

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -3,62 +3,64 @@ import { useState } from "react";
 
 import { registerUser } from "../api/auth";
 
-const Register = ({setToken}) => {
+const Register = ({ setToken }) => {
     const [username, setUsername] =
-    useState("");
+        useState("");
     const [password, setPassword] =
-    useState("");
+        useState("");
     const [passwordAuth, setPasswordAuth] =
-    useState("");
+        useState("");
     const [passwordError, setPasswordError] =
-    useState("");
+        useState("");
     return (
         <div className="register-bar">
             <form
-               onSubmit={async (e) => {
-                try {
-                    if (password === passwordAuth) {
-                        e.preventDefault();
-                        console.log(password, username);
-                        const token = await registerUser(username, password);
-                        setToken(token);
-                        console.log(token);
-                        localStorage.setItem("token", token);
-                        setUsername("");
-                        setPassword("");
-                        setPasswordAuth("");
-                        setPasswordError("");
-                    } else {
-                        e.preventDefault();
-                        setPasswordError("Error: Passwords do not match.")
+                onSubmit={async (e) => {
+                    try {
+                        if (password === passwordAuth) {
+                            e.preventDefault();
+                            console.log(password, username);
+                            const token = await registerUser(username, password);
+                            setToken(token);
+                            console.log(token);
+                            localStorage.setItem("token", token);
+                            setUsername("");
+                            setPassword("");
+                            setPasswordAuth("");
+                            setPasswordError("");
+                        } else {
+                            e.preventDefault();
+                            setPasswordError("Error: Passwords do not match.")
+                        }
+                    } catch (error) {
+                        console.error()
                     }
-                }   catch(error)   {
-                    console.error()
-                }
-               }}
+                }}
             >
                 <label htmlFor="username">New Username: </label>
-                <input 
-                  value={username}
-                  type="text"
-                  placeholder="Username"
-                  onChange={(e) => setUsername(e.target.value)}
+                <input
+                    value={username}
+                    type="text"
+                    placeholder="Username"
+                    onChange={(e) => setUsername(e.target.value)}
                 ></input>
 
                 <label htmlFor="password">New Password: </label>
-                <input 
-                  value={password}
-                  type="password"
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
+                <input
+                    value={password}
+                    minLength={8}
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
                 ></input>
 
                 <label htmlFor="password">Confirm New Password: </label>
-                <input 
-                  value={passwordAuth}
-                  type="password"
-                  placeholder="Confirm Password"
-                  onChange={(e) => setPasswordAuth(e.target.value)}
+                <input
+                    value={passwordAuth}
+                    minLength={8}
+                    type="password"
+                    placeholder="Confirm Password"
+                    onChange={(e) => setPasswordAuth(e.target.value)}
                 ></input>
 
                 <button type="submit">Submit</button>
